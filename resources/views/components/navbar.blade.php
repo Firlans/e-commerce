525d0552dcf6446cac506929f56dcd866a7c6b59
Firlans <firlansyah54321@gmail.com>
2024-10-17 11:21:42 +0700

    membuat view contact pages dan component navbar dengan blade di view dan menambahkan route /contact

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="flex-row">
+    <h1>Firlan Book Store</h1>
+    <div>
+
+        <input type="text" name="search">
+    </div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">contact</a></li>
+    </ul>
+</nav>
